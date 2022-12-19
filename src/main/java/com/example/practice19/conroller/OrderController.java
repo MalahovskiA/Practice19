a5f35f2531d54129d5db71b045c6f68e0672b266
Malahovski.A <malahovski82@gmail.com>
2022-12-19 21:30:17 +0300

    Update

--- src/main/java/com/example/practice19/conroller/OrderController.java
+++ src/main/java/com/example/practice19/conroller/OrderController.java
@@ -4,16 +4,14 @@ import com.example.practice19.model.Invite;
 import com.example.practice19.model.Order;
 import com.example.practice19.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
